242dbda5f230babdd4a424fe75d3b8395ff51074
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-06-29 17:35:42 +0900

    [ADD] #1 - Init DI(Koin)

---
+++ app/src/main/java/org/mascota/MascotaApplication.kt
@@ -0,0 +1,15 @@
+package org.mascota
+
+import android.app.Application
+import org.mascota.di.viewModelModule
+import org.mascota.util.extension.setUpKoin
+
+class MascotaApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        setUpKoin(
+            this,
+            viewModelModule
+        )
+    }
+}
