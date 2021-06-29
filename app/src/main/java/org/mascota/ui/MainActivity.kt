ee029c8bfc95671aabd794fa5be946869d8f6415
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-06-29 17:37:51 +0900

    [ADD] #1 - Init Main Activity(BottomNavigation)

---
+++ app/src/main/java/org/mascota/ui/MainActivity.kt
@@ -0,0 +1,49 @@
+package org.mascota.ui
+
+import com.valuecruncher.gachisaja.base.BindingActivity
+import org.mascota.R
+import org.mascota.databinding.ActivityMainBinding
+import org.mascota.ui.view.calendar.CalendarFragment
+import org.mascota.ui.view.home.HomeFragment
+import org.mascota.ui.view.rainbow.RainbowFragment
+import org.mascota.util.extension.replace
+
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private val homeFragment: HomeFragment by lazy { HomeFragment() }
+    private val calendarFragment: CalendarFragment by lazy { CalendarFragment() }
+    private val rainbowFragment: RainbowFragment by lazy { RainbowFragment() }
+
+    override fun initView() {
+        initSelectMenu()
+        replaceHomeFragment()
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        binding.bnvMain.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_calendar -> {
+                    replaceCalendarFragment()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.menu_home -> {
+                    replaceHomeFragment()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.menu_rainbow -> {
+                    replaceRainbowFragment()
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+    }
+
+    private fun initSelectMenu() { binding.bnvMain.selectedItemId = R.id.menu_home }
+
+    private fun replaceCalendarFragment() { replace(R.id.container_main, calendarFragment) }
+
+    private fun replaceHomeFragment() { replace(R.id.container_main, homeFragment) }
+
+    private fun replaceRainbowFragment() { replace(R.id.container_main, rainbowFragment) }
+}
