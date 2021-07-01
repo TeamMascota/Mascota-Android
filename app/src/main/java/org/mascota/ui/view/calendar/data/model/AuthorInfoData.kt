3d8c68e5e6b4bbe3a5e5e46caf8bcd9f9fa21355
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-07-02 02:13:57 +0900

    [ADD] #3 - Add Data Logic

---
+++ app/src/main/java/org/mascota/ui/view/calendar/data/model/AuthorInfoData.kt
@@ -0,0 +1,7 @@
+package org.mascota.ui.view.calendar.data.model
+
+data class AuthorInfoData(
+    val name: String,
+    val petName: String,
+    val recordNum: String
+)
--- app/src/main/java/org/mascota/ui/viewmodel/CalendarViewModel.kt
+++ app/src/main/java/org/mascota/ui/viewmodel/CalendarViewModel.kt
@@ -1,7 +1,26 @@
 package org.mascota.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import org.mascota.ui.view.calendar.data.datasource.AuthorDataSource
+import org.mascota.ui.view.calendar.data.model.AuthorInfoData
 
-class CalendarViewModel : ViewModel() {
+class CalendarViewModel(private val authorDataSource: AuthorDataSource) : ViewModel() {
+    private val _authorInfo = MutableLiveData<AuthorInfoData>()
+    val authorInfo: LiveData<AuthorInfoData>
+        get() = _authorInfo
+
+    fun getAuthorInfo() = viewModelScope.launch {
+        runCatching { authorDataSource.getAuthorInfoData() }
+            .onSuccess {
+                _authorInfo.postValue(it)
+            }
+            .onFailure {
+                it.printStackTrace()
+            }
+    }
 
 }
