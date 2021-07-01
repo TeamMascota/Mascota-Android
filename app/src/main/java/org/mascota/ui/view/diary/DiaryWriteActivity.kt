563c7c7eab797e4b57341d9c37a23040d32c5390
ssonghyun101 <songhyun0868@gmail.com>
2021-07-01 19:52:45 +0900

    [ADD] #7 - Add DiaryWriteActivity

---
+++ app/src/main/java/org/mascota/ui/view/diary/DiaryWriteActivity.kt
@@ -0,0 +1,12 @@
+package org.mascota.ui.view.diary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.mascota.R
+
+class DiaryWriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary_write)
+    }
+}
