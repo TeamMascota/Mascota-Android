74f67b7ba6f32d628d9cf96912ca34d2a03aa564
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-07-03 21:06:48 +0900

    [ADD] #10 - Add Switch Btn Click Listener

--- app/src/main/java/org/mascota/ui/view/calendar/CalendarFragment.kt
+++ app/src/main/java/org/mascota/ui/view/calendar/CalendarFragment.kt
@@ -1,11 +1,41 @@
 package org.mascota.ui.view.calendar
 
+import android.content.Intent
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.mascota.R
 import org.mascota.databinding.FragmentCalendarBinding
 import org.mascota.ui.base.BindingFragment
+import org.mascota.ui.viewmodel.CalendarViewModel
 
 class CalendarFragment : BindingFragment<FragmentCalendarBinding>(R.layout.fragment_calendar) {
-    override fun initView() {
+    private val calendarViewModel: CalendarViewModel by viewModel()
 
+    override fun initView() {
+        binding.tvSwitchPart1.setOnClickListener {
+            it.isSelected = !it.isSelected
+        }
+        initClickEvent()
+        getAuthorInfo()
+        observeAuthorInfo()
+    }
+
+    private fun getAuthorInfo() {
+        calendarViewModel.getAuthorInfo()
+    }
+
+    private fun observeAuthorInfo() {
+        calendarViewModel.authorInfo.observe(viewLifecycleOwner) {
+            binding.authorInfoData = it
+        }
+    }
+
+    private fun initClickEvent() {
+        binding.clRecord.setOnClickListener {
+            startDiaryWriteActivity()
+        }
+    }
+
+    private fun startDiaryWriteActivity() {
+        //startActivity(Intent(requireActivity(), DiaryWriteActivity::class.java))
     }
 }
