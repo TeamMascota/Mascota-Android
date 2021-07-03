12ca2851a8605c18080a01ed57a06fd4be5e7561
onpyeong <gsh4471@gmail.com>
2021-07-04 00:20:52 +0900

    [ADD]  #8 - Init DataSourceModule

--- app/src/main/java/org/mascota/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/org/mascota/ui/viewmodel/HomeViewModel.kt
@@ -1,7 +1,27 @@
 package org.mascota.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import org.mascota.ui.view.home.data.datasource.HomeBookDataSource
+import org.mascota.ui.view.home.data.model.HomeBookInfoData
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(private val homeBookDataSource: HomeBookDataSource) : ViewModel() {
+    private val _homeBookInfo = MutableLiveData<HomeBookInfoData>()
+    val homeBookInfo: LiveData<HomeBookInfoData>
+    get() = _homeBookInfo
+
+    fun getHomeBookInfo() = viewModelScope.launch {
+        runCatching { homeBookDataSource.getHomeBookInfoData() }
+            .onSuccess {
+                _homeBookInfo.postValue(it)
+            }
+            .onFailure {
+                it.printStackTrace()
+            }
+    }
+
 
 }
