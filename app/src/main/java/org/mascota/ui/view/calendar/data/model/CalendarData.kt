89333fd9337c653ed091a5908d778707de6433a3
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-07-04 23:57:59 +0900

    [ADD] #10 - Add Data Logic

---
+++ app/src/main/java/org/mascota/ui/view/calendar/data/model/CalendarData.kt
@@ -0,0 +1,6 @@
+package org.mascota.ui.view.calendar.data.model
+
+data class CalendarData(
+    val Emo : Int,
+    val recordSize : Int
+)
--- app/src/main/java/org/mascota/ui/viewmodel/CalendarViewModel.kt
+++ app/src/main/java/org/mascota/ui/viewmodel/CalendarViewModel.kt
@@ -7,11 +7,18 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import org.mascota.ui.view.calendar.data.datasource.AuthorDataSource
 import org.mascota.ui.view.calendar.data.model.AuthorInfoData
+import org.mascota.util.CalendarUtil.initCalendar
+import java.util.*
 
 class CalendarViewModel(private val authorDataSource: AuthorDataSource) : ViewModel() {
+    private val nowCalendar = initCalendar(Calendar.getInstance(Locale.KOREA))
     private val _authorInfo = MutableLiveData<AuthorInfoData>()
     val authorInfo: LiveData<AuthorInfoData>
         get() = _authorInfo
+
+    private val _curCalendar = MutableLiveData<Calendar>()
+    val curCalendar: LiveData<Calendar>
+        get() = _curCalendar
 
     fun getAuthorInfo() = viewModelScope.launch {
         runCatching { authorDataSource.getAuthorInfoData() }
@@ -23,4 +30,32 @@ class CalendarViewModel(private val authorDataSource: AuthorDataSource) : ViewMo
             }
     }
 
+    fun setCalendar() {
+        _curCalendar.postValue(nowCalendar)
+    }
+
+    fun addMonth() {
+        nowCalendar.add(Calendar.MONTH, NEXT)
+        setCalendar()
+    }
+
+    fun minusMonth() {
+        nowCalendar.add(Calendar.MONTH, PREV)
+        setCalendar()
+    }
+
+    fun addYear() {
+        nowCalendar.add(Calendar.YEAR, NEXT)
+        setCalendar()
+    }
+
+    fun minusYear() {
+        nowCalendar.add(Calendar.YEAR, PREV)
+        setCalendar()
+    }
+
+    companion object {
+        const val PREV = -1
+        const val NEXT = 1
+    }
 }
