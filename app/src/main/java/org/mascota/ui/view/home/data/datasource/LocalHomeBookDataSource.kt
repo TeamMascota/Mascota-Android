c2593f25022cf3dd491cf127c858b1236278477b
onpyeong <gsh4471@gmail.com>
2021-07-05 19:32:27 +0900

    [CHORE] #12 - apply dp to px function

--- app/src/main/java/org/mascota/ui/view/home/data/datasource/LocalHomeBookDataSource.kt
+++ app/src/main/java/org/mascota/ui/view/home/data/datasource/LocalHomeBookDataSource.kt
@@ -7,5 +7,5 @@ import org.mascota.ui.view.home.data.model.HomePageInfoData
 class LocalHomeBookDataSource : HomeBookDataSource {
     override fun getHomeBookInfoData(): HomeBookInfoData = HomeBookInfoData("1부 ", "모여봐요 코봉의 숲", "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_960_720.jpg")
     override fun getHomeDiaryInfoData(): HomeDiaryInfoData = HomeDiaryInfoData("162화", "제목", "내용", "2020.02.20")
-    override fun getHomePageInfoData(): HomePageInfoData = HomePageInfoData(prolog = true)
+    override fun getHomePageInfoData(): HomePageInfoData = HomePageInfoData(prolog = false)
 }
--- app/src/main/java/org/mascota/util/BindingAdapter.kt
+++ app/src/main/java/org/mascota/util/BindingAdapter.kt
@@ -1,9 +1,11 @@
 package org.mascota.util
 
+import android.annotation.SuppressLint
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import org.mascota.R
 import org.mascota.ui.view.calendar.CalendarFragment.Companion.DOG_ANGRY
 import org.mascota.ui.view.calendar.CalendarFragment.Companion.DOG_BORING
@@ -62,5 +64,18 @@ object BindingAdapter {
                 else -> View.GONE
             }
         }
+    }
+
+    @JvmStatic
+    @BindingAdapter("imgUrl")
+    fun setRemoteCircleImage(image: ImageView, url: String?) {
+        Glide.with(image.context).load(url).into(image)
+    }
+
+    @SuppressLint("SetTextI18n")
+    @JvmStatic
+    @BindingAdapter("TitleText")
+    fun setEpisodeText(textView: TextView, number: Int?) {
+        textView.text = number.toString() + "화 기록하기"
     }
 }
