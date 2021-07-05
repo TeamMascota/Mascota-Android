80fe12f9111ecd0d04484ba3606dcc962892d87e
onpyeong <gsh4471@gmail.com>
2021-07-05 19:03:14 +0900

    [ADD] #12 - Add Binding Adapter

--- app/src/main/java/org/mascota/ui/view/home/BindingConversions.kt
+++ app/src/main/java/org/mascota/ui/view/home/BindingConversions.kt
@@ -1,6 +1,8 @@
 package org.mascota.ui.view.home
 
+import android.annotation.SuppressLint
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
@@ -9,5 +11,12 @@ object BindingConversions {
     @BindingAdapter("imgUrl")
     fun setRemoteCircleImage(image: ImageView, url: String?) {
         Glide.with(image.context).load(url).into(image)
+    }
+
+    @SuppressLint("SetTextI18n")
+    @JvmStatic
+    @BindingAdapter("TitleText")
+    fun setEpisodeText(textView: TextView, number: Int?) {
+        textView.text = number.toString() + "화 기록하기"
     }
 }
