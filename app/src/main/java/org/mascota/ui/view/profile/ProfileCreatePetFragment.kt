beb513fd8af2e0d1c17d1bdaa5cc6c03a2b21f01
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-07-06 23:10:27 +0900

    [ADD] #13 - Init CreatePetFragment

---
+++ app/src/main/java/org/mascota/ui/view/profile/ProfileCreatePetFragment.kt
@@ -0,0 +1,16 @@
+package org.mascota.ui.view.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.mascota.R
+import org.mascota.databinding.FragmentProfileCreatePetBinding
+import org.mascota.ui.base.BindingFragment
+
+class ProfileCreatePetFragment : BindingFragment<FragmentProfileCreatePetBinding>(R.layout.fragment_profile_create_pet) {
+    override fun initView() {
+
+    }
+}
