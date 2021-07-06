d38ec5daba07bc7d6ab304d758dc4e0d4014e502
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-07-06 23:09:41 +0900

    [ADD] #13 - Init CreateBookFragment

---
+++ app/src/main/java/org/mascota/ui/view/profile/ProfileCreateBookFragment.kt
@@ -0,0 +1,11 @@
+package org.mascota.ui.view.profile
+
+import org.mascota.R
+import org.mascota.databinding.FragmentProfileCreateBookBinding
+import org.mascota.ui.base.BindingFragment
+
+class ProfileCreateBookFragment : BindingFragment<FragmentProfileCreateBookBinding>(R.layout.fragment_profile_create_book) {
+    override fun initView() {
+
+    }
+}
