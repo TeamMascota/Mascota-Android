21bdc28663320f3ba746518498fb7e7423b6f841
ssonghyun101 <songhyun0868@gmail.com>
2021-07-07 03:07:45 +0900

    [CHORE] #7 - select Gallery

--- app/src/main/java/org/mascota/ui/view/diary/DiaryDetailWriteFragment.kt
+++ app/src/main/java/org/mascota/ui/view/diary/DiaryDetailWriteFragment.kt
@@ -1,15 +1,25 @@
 package org.mascota.ui.view.diary
 
+import android.Manifest
+import android.app.Activity
 import android.app.DatePickerDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.provider.MediaStore
 import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import org.mascota.R
 import org.mascota.databinding.FragmentDiaryDetailWriteBinding
 import org.mascota.ui.base.BindingFragment
 import java.util.*
 
 
+@Suppress("DEPRECATION")
 class DiaryDetailWriteFragment : BindingFragment<FragmentDiaryDetailWriteBinding>(R.layout.fragment_diary_detail_write) {
     val items = arrayOf("아이템1", "아이템2")
+    val REQ_STORAGE_PERMISSION : Int = 1
+    val REQ_GALLERY : Int = 1
 
 
     override fun initView() {
@@ -94,6 +104,7 @@ class DiaryDetailWriteFragment : BindingFragment<FragmentDiaryDetailWriteBinding
         val bottomSheet = BottomSheet()
         bottomSheet.setCallbackButtonClickListener {
             // 카메라 함수 실행하기
+            selectGallery()
             Log.d("프래그멘트","누름")
         }
         activity?.supportFragmentManager?.let { it1 ->
@@ -136,4 +147,32 @@ class DiaryDetailWriteFragment : BindingFragment<FragmentDiaryDetailWriteBinding
 
         dpd.show()
     }
-}
+
+
+    private fun selectGallery(){
+        var writePermission = context?.let { ContextCompat.checkSelfPermission(it, Manifest.permission.WRITE_EXTERNAL_STORAGE) }
+        var readPermission = context?.let{ ContextCompat.checkSelfPermission(it,Manifest.permission.READ_EXTERNAL_STORAGE)}
+
+        if(writePermission == PackageManager.PERMISSION_DENIED || readPermission == PackageManager.PERMISSION_DENIED)
+        {
+            //권한이 없을 경우 요청함
+            context?.let{
+                ActivityCompat.requestPermissions(
+                    it as Activity, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE),
+                    REQ_STORAGE_PERMISSION)
+            }
+
+        }
+        else{
+            //권한 있을 경우
+            var intent = Intent(Intent.ACTION_PICK)
+            intent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            intent.type = "image/*"
+            startActivityForResult(intent,REQ_GALLERY)
+        }
+
+
+        }
+
+    }
+
