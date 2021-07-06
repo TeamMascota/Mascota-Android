cc5a244693e17be63345ce14f5b35802539ea50b
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-07-06 23:09:20 +0900

    [ADD] #13 - Init ProfileCreateActivity

---
+++ app/src/main/java/org/mascota/ui/view/profile/ProfileCreateActivity.kt
@@ -0,0 +1,11 @@
+package org.mascota.ui.view.profile
+
+import org.mascota.R
+import org.mascota.databinding.ActivityProfileCreateBinding
+import org.mascota.ui.base.BindingActivity
+
+class ProfileCreateActivity : BindingActivity<ActivityProfileCreateBinding>(R.layout.activity_profile_create) {
+    override fun initView() {
+
+    }
+}
