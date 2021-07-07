eb5e6df3f11cbb6612b98fe9000432d2cf8558e8
onpyeong <70002218+onpyeong@users.noreply.github.com>
2021-07-07 18:20:37 +0900

    [FEAT] #20 - Nested RecyclerView

--- app/src/main/java/org/mascota/ui/view/content/detail/ContentDetailActivity.kt
+++ app/src/main/java/org/mascota/ui/view/content/detail/ContentDetailActivity.kt
@@ -4,18 +4,14 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.mascota.R
 import org.mascota.databinding.ActivityContentDetailBinding
 import org.mascota.ui.base.BindingActivity
-import org.mascota.ui.view.content.detail.adapter.ContentDetailDiaryAdapter
 import org.mascota.ui.view.content.detail.adapter.ContentDetailMonthAdapter
 import org.mascota.ui.viewmodel.ContentDetailViewModel
 
 class ContentDetailActivity : BindingActivity<ActivityContentDetailBinding>(R.layout.activity_content_detail) {
     private val contentDetailViewModel : ContentDetailViewModel by viewModel()
     private lateinit var contentDetailMonthAdapter: ContentDetailMonthAdapter
-    private lateinit var contentDetailDiaryAdapter: ContentDetailDiaryAdapter
 
     override fun initView() {
-        getContentDiaryInfo()
-        observeContentDiaryInfo()
         getContentMonthInfo()
         initContentDetailMonthAdapter()
         observeContentDetailMonthInfo()
@@ -28,22 +24,11 @@ class ContentDetailActivity : BindingActivity<ActivityContentDetailBinding>(R.la
     private fun initContentDetailMonthAdapter() {
         contentDetailMonthAdapter = ContentDetailMonthAdapter()
         binding.rvContentMonth.adapter = contentDetailMonthAdapter
-        //binding.rvContent.isNestedScrollingEnabled = false
     }
 
     private fun observeContentDetailMonthInfo() {
         contentDetailViewModel.contentDetailMonth.observe(this) {
             contentDetailMonthAdapter.contentMonthList = it
-        }
-    }
-
-    private fun getContentDiaryInfo() {
-        contentDetailViewModel.getContentDetailInfo()
-    }
-
-    private fun observeContentDiaryInfo() {
-        contentDetailViewModel.contentDetail.observe(this) {
-            contentDetailDiaryAdapter.contentDiaryList = it
         }
     }
 }
--- app/src/main/java/org/mascota/ui/view/content/detail/adapter/ContentDetailMonthAdapter.kt
+++ app/src/main/java/org/mascota/ui/view/content/detail/adapter/ContentDetailMonthAdapter.kt
@@ -6,7 +6,8 @@ import androidx.recyclerview.widget.RecyclerView
 import org.mascota.databinding.ItemDiaryMonthBinding
 import org.mascota.ui.view.content.detail.data.model.ContentMonthInfoData
 
-class ContentDetailMonthAdapter : RecyclerView.Adapter<ContentDetailMonthAdapter.ContentDetailMonthViewHolder>() {
+class ContentDetailMonthAdapter :
+    RecyclerView.Adapter<ContentDetailMonthAdapter.ContentDetailMonthViewHolder>() {
 
     private val _contentMonthList = mutableListOf<ContentMonthInfoData>()
 
@@ -17,7 +18,10 @@ class ContentDetailMonthAdapter : RecyclerView.Adapter<ContentDetailMonthAdapter
             notifyDataSetChanged()
         }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContentDetailMonthViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ContentDetailMonthViewHolder {
         val binding = ItemDiaryMonthBinding.inflate(
             LayoutInflater.from(parent.context), parent, false
         )
@@ -26,19 +30,22 @@ class ContentDetailMonthAdapter : RecyclerView.Adapter<ContentDetailMonthAdapter
 
     override fun onBindViewHolder(holder: ContentDetailMonthViewHolder, position: Int) {
         holder.onBind(contentMonthList[position])
-        contentDetailDiaryAdapter = ContentDetailDiaryAdapter()//
-        holder.binding.rvContentDiary.adapter = contentDetailDiaryAdapter//
+
+
     }
 
     override fun getItemCount(): Int = contentMonthList.size
 
-    private lateinit var contentDetailDiaryAdapter: ContentDetailDiaryAdapter //
 
     class ContentDetailMonthViewHolder(
         val binding: ItemDiaryMonthBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         fun onBind(contentMonthInfoData: ContentMonthInfoData) {
             binding.contentMonthDataInfo = contentMonthInfoData
+
+            val contentDetailDiaryAdapter: ContentDetailDiaryAdapter = ContentDetailDiaryAdapter()
+            contentDetailDiaryAdapter.contentDiaryList = contentMonthInfoData.diaryList
+            binding.rvContentDiary.adapter = contentDetailDiaryAdapter
         }
     }
 }
