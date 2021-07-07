e85ad3d36077a28bc1d7d8555e448c7c1c1830f5
onpyeong <70002218+onpyeong@users.noreply.github.com>
2021-07-08 00:58:24 +0900

    [REFACTOR] #24 - refactor file

--- app/src/main/java/org/mascota/ui/view/content/edit/ContentEditActivity.kt
+++ app/src/main/java/org/mascota/ui/view/content/edit/ContentEditActivity.kt
@@ -1,7 +1,5 @@
 package org.mascota.ui.view.content.edit
 
-import android.app.AlertDialog
-import android.view.LayoutInflater
 import org.mascota.R
 import org.mascota.databinding.ActivityContentEditBinding
 import org.mascota.ui.base.BindingActivity
@@ -9,16 +7,16 @@ import org.mascota.ui.view.content.edit.adapter.ContentEditAdapter
 import org.mascota.ui.view.content.edit.data.datasource.LocalContentEditDataSource
 
 class ContentEditActivity : BindingActivity<ActivityContentEditBinding>(R.layout.activity_content_edit) {
-    private lateinit var contentEditAdapter: ContentEditAdapter
 
     override fun initView() {
         initContentEditAdapter()
     }
 
     private fun initContentEditAdapter() {
-        contentEditAdapter = ContentEditAdapter()
-        binding.rvContent.adapter = contentEditAdapter
-        val localContentEditDataSource = LocalContentEditDataSource()
-        contentEditAdapter.contentEditList = localContentEditDataSource.getContentEditInfoData()
+        ContentEditAdapter().apply {
+            binding.rvContent.adapter = this
+            val localContentEditDataSource = LocalContentEditDataSource()
+            contentEditList = localContentEditDataSource.getContentEditInfoData()
+        }
     }
 }
