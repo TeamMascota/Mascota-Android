d44af20f6748574ca668d3156c4ae83a4f87844e
ssonghyun101 <songhyun0868@gmail.com>
2021-07-07 18:30:45 +0900

    [CHORE] #7 - Title TextCount

--- app/src/main/java/org/mascota/ui/view/diary/DiaryDetailWriteFragment.kt
+++ app/src/main/java/org/mascota/ui/view/diary/DiaryDetailWriteFragment.kt
@@ -1,33 +1,48 @@
 package org.mascota.ui.view.diary
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
-import android.app.DatePickerDialog
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import android.provider.MediaStore
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import org.mascota.R
 import org.mascota.databinding.FragmentDiaryDetailWriteBinding
 import org.mascota.ui.base.BindingFragment
-import java.util.*
+import java.time.LocalDate
+import java.time.LocalDate.now
+import java.time.format.DateTimeFormatter
 
 
 @Suppress("DEPRECATION")
 class DiaryDetailWriteFragment : BindingFragment<FragmentDiaryDetailWriteBinding>(R.layout.fragment_diary_detail_write) {
     val items = arrayOf("아이템1", "아이템2")
-    val REQ_STORAGE_PERMISSION : Int = 1
-    val REQ_GALLERY : Int = 1
+    val REQ_STORAGE_PERMISSION: Int = 1
+    val REQ_GALLERY = 12
+    @RequiresApi(Build.VERSION_CODES.O)
+    val current_date : LocalDate = now()
+    @RequiresApi(Build.VERSION_CODES.O)
+    val formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
+    @RequiresApi(Build.VERSION_CODES.O)
+    val tv_today = current_date.format(formatter).toString()
 
 
     override fun initView() {
         initClickSelectImage()
+        binding.tvToday.setText(tv_today)
+        WriteTitle()
 
-        binding.btnPickerDate.setOnClickListener {
-            showDatePicker()
-        }
+
+
+
 
         /*
 
@@ -75,10 +90,10 @@ class DiaryDetailWriteFragment : BindingFragment<FragmentDiaryDetailWriteBinding
     }
 
 
-    private fun initClickSelectImage(){
+    private fun initClickSelectImage() {
 
 
-        with(binding){
+        with(binding) {
             iv1.setOnClickListener {
                 showBottomSheet()
             }
@@ -100,12 +115,12 @@ class DiaryDetailWriteFragment : BindingFragment<FragmentDiaryDetailWriteBinding
 
     }
 
-    private fun showBottomSheet(){
+    private fun showBottomSheet() {
         val bottomSheet = BottomSheet()
         bottomSheet.setCallbackButtonClickListener {
             // 카메라 함수 실행하기
             selectGallery()
-            Log.d("프래그멘트","누름")
+            Log.d("프래그멘트", "누름")
         }
         activity?.supportFragmentManager?.let { it1 ->
             bottomSheet.show(it1, bottomSheet.tag)
@@ -115,64 +130,107 @@ class DiaryDetailWriteFragment : BindingFragment<FragmentDiaryDetailWriteBinding
     }
 
 
+    private fun selectGallery() {
+        var writePermission = context?.let {
+            ContextCompat.checkSelfPermission(
+                it,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        }
+        var readPermission = context?.let {
+            ContextCompat.checkSelfPermission(
+                it,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        }
 
-
-    private fun showDatePicker(){
-        val datePicker = Calendar.getInstance()
-        val year = datePicker.get(Calendar.YEAR)
-        val month = datePicker.get(Calendar.MONTH)
-        val day = datePicker.get(Calendar.DAY_OF_MONTH)
-
-        val dpd = DatePickerDialog(
-
-            requireContext(),
-            R.style.MySpinnerDatePickerStyle,
-            {_, year, monthOfYear, dayOfMonth ->
-                //월이 0부터 시작하여 1을 더해줘야힘
-                val month = monthOfYear + 1
-                //선택한 날짜의 요일을 구하기 위한 캘린더
-                val calendar = Calendar.getInstance()
-                // 선택한 날짜 세팅
-                calendar.set(year,monthOfYear,dayOfMonth)
-
-
-                binding.btnPickerDate.text = "$year.$month.$dayOfMonth"
-
-
-            },
-            year,
-            month,
-            day
-        )
-
-        dpd.show()
-    }
-
-
-    private fun selectGallery(){
-        var writePermission = context?.let { ContextCompat.checkSelfPermission(it, Manifest.permission.WRITE_EXTERNAL_STORAGE) }
-        var readPermission = context?.let{ ContextCompat.checkSelfPermission(it,Manifest.permission.READ_EXTERNAL_STORAGE)}
-
-        if(writePermission == PackageManager.PERMISSION_DENIED || readPermission == PackageManager.PERMISSION_DENIED)
-        {
+        if (writePermission == PackageManager.PERMISSION_DENIED || readPermission == PackageManager.PERMISSION_DENIED) {
             //권한이 없을 경우 요청함
-            context?.let{
+            context?.let {
                 ActivityCompat.requestPermissions(
-                    it as Activity, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE),
-                    REQ_STORAGE_PERMISSION)
+                    it as Activity,
+                    arrayOf(
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ),
+                    REQ_STORAGE_PERMISSION
+                )
             }
 
-        }
-        else{
+        } else {
             //권한 있을 경우
             var intent = Intent(Intent.ACTION_PICK)
-            intent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-            intent.type = "image/*"
-            startActivityForResult(intent,REQ_GALLERY)
+            //어떤 종류의 데이터를 선택할 수 있는지 정해줌
+            // intent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            // intent.type = "image/*"
+            intent.type = MediaStore.Images.Media.CONTENT_TYPE
+            startActivityForResult(intent, REQ_GALLERY)
+
         }
+    }
 
 
+
+
+
+    //갤러리에서 사진 선택한 후 호출
+    //결과값이 넘어옴
+    //인탠트로 이미지가 넘어옴
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d("됨","됨")
+        if(requestCode == RESULT_OK){
+            when(requestCode){
+                REQ_GALLERY->{
+                    // 이미지가 nullable함. null일 경우 처리해야함
+                    data?.data?.let {
+                        uri ->
+                        binding.iv1.setImageURI(uri)
+
+                    }
+//data에 uri 주소가 담겨서 그냥 꺼내면됨
+                // 원본 이미지의 주소를 저장할 uri 필요
+
+
+
+                }
+
+            }
         }
 
     }
 
+    private fun WriteTitle() {
+        binding.etTitle.addTextChangedListener(object : TextWatcher{
+            @SuppressLint("SetTextI18n")
+            override fun afterTextChanged(p0: Editable?) {
+                var userinput = binding.etTitle.text.toString()
+                binding.tvCount.text = userinput.length.toString() + " /11"
+
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.tvCount.text = "0/11"
+
+            }
+
+            @SuppressLint("SetTextI18n")
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                var userinput = binding.etTitle.text.toString()
+                binding.tvCount.text = userinput.length.toString() + " /11"
+            }
+
+
+        })
+
+    }
+
+
+
+
+
+
+
+}
