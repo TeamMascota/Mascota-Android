2287e342ba5b4213fd2129bcb548a61c64750f1c
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-07-07 16:13:22 +0900

    [ADD] #13 - Add Utill Class(Animation, Calendar, String)

---
+++ app/src/main/java/org/mascota/util/StringUtil.kt
@@ -0,0 +1,9 @@
+package org.mascota.util
+
+object StringUtil {
+    fun makeHeroNumbering(position: Int): String = "주인공 $position"
+
+    fun makeHeroNumberingAndName(position: Int): String = "주인공 ${position}의 이름"
+
+    fun makeTextLength(length : Int) : String = "($length"
+}
