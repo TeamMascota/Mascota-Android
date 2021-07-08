c8212651ae8fd805842eaa98c1501e16fb79e22f
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-07-08 23:53:19 +0900

    [ADD] #29 - Init Farewell Book Fragment

---
+++ app/src/main/java/org/mascota/ui/view/rainbow/farewell/FarewellBookFragment.kt
@@ -0,0 +1,17 @@
+package org.mascota.ui.view.rainbow.farewell
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.mascota.R
+import org.mascota.databinding.FragmentFarewellBookBinding
+import org.mascota.ui.base.BindingFragment
+
+class FarewellBookFragment : BindingFragment<FragmentFarewellBookBinding>(R.layout.fragment_farewell_book) {
+    override fun initView() {
+
+    }
+
+}
