1b54acf1ea65999e9d539d8e1741da276996cab2
ssonghyun101 <songhyun0868@gmail.com>
2021-07-10 00:18:12 +0900

    [ADD] #33 - Add View

---
+++ app/src/main/java/org/mascota/ui/view/rainbow/farewell/FarewellBestMoment.kt
@@ -0,0 +1,25 @@
+package org.mascota.ui.view.rainbow.farewell
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.mascota.R
+
+
+
+class FarewellBestMoment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_farewell_best_moment, container, false)
+    }
+
+
+
+
+}
