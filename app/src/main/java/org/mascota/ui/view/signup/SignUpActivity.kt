b0f28ece8bd13705fe03a710312f59df6a2f06da
ssonghyun101 <songhyun0868@gmail.com>
2021-07-10 23:34:08 +0900

    [ADD] #44-Init SignUpAcivity

---
+++ app/src/main/java/org/mascota/ui/view/signup/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package org.mascota.ui.view.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
