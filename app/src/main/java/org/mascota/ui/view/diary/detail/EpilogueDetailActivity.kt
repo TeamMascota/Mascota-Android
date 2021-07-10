ec82a6ccfde9f7d9de57b44a8e7b1d4046dc39cd
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-07-11 04:39:23 +0900

    [CHORE] #43 - Add StatusBarColor and Manifest

--- app/src/main/java/org/mascota/ui/view/diary/detail/EpilogueDetailActivity.kt
+++ app/src/main/java/org/mascota/ui/view/diary/detail/EpilogueDetailActivity.kt
@@ -3,9 +3,11 @@ package org.mascota.ui.view.diary.detail
 import org.mascota.R
 import org.mascota.databinding.ActivityEpilogueDetailBinding
 import org.mascota.ui.base.BindingActivity
+import org.mascota.util.StatusBarUtil.setStatusBarColor
 
-class EpilogueDetailActivity : BindingActivity<ActivityEpilogueDetailBinding>(R.layout.activity_epilogue_write) {
+class EpilogueDetailActivity : BindingActivity<ActivityEpilogueDetailBinding>(R.layout.activity_epilogue_detail) {
     override fun initView() {
+        setStatusBarColor(getColor(R.color.maco_orange))
         initText()
     }
 
