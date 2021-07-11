892d83be75f1ae61c1604334048fb8b3cbe64abd
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-07-11 23:25:04 +0900

    [ADD] #45 - Init Diary Solution Fragment

---
+++ app/src/main/java/org/mascota/ui/view/diary/DiarySolutionFragment.kt
@@ -0,0 +1,12 @@
+package org.mascota.ui.view.diary
+
+import org.mascota.R
+import org.mascota.databinding.FragmentDiarySolutionBinding
+import org.mascota.ui.base.BindingFragment
+
+class DiarySolutionFragment : BindingFragment<FragmentDiarySolutionBinding>(R.layout.fragment_diary_solution) {
+    override fun initView() {
+
+    }
+
+}
