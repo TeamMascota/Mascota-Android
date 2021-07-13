22b9068ee65716c030edd3bdb666704dc266e218
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-07-13 13:34:19 +0900

    [ADD] #51 - Init DiaryRepository

---
+++ app/src/main/java/org/mascota/data/repository/diary/DiaryRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package org.mascota.data.repository.diary
+
+import org.mascota.data.remote.datasource.diary.DiaryDataSource
+import org.mascota.data.remote.model.request.diary.ReqDiaryDelete
+import org.mascota.data.remote.model.request.diary.ReqPersonDiaryEdit
+import org.mascota.data.remote.model.request.diary.ReqPersonDiaryWrite
+import org.mascota.data.remote.model.request.diary.ReqPetDiaryWrite
+import org.mascota.data.remote.model.response.diary.*
+
+class DiaryRepositoryImpl(private val remoteDataSource : DiaryDataSource) : DiaryRepository{
+    override suspend fun postPetDiaryWrite(body: ReqPetDiaryWrite): ResPetDiaryWrite = remoteDataSource.postPetDiaryWrite(body)
+
+    override suspend fun postPetDiaryRead(path: String): ResDiaryRead = remoteDataSource.postPetDiaryRead(path)
+
+    override suspend fun putPetDiaryEdit(path: String, body: ReqPetDiaryWrite): ResDiaryEdit = remoteDataSource.putPetDiaryEdit(path, body)
+
+    override suspend fun deletePetDiary(path: String, body: ReqDiaryDelete): ResDiaryDelete = remoteDataSource.deletePetDiary(path, body)
+
+    override suspend fun postPersonDiaryWrite(body: ReqPersonDiaryWrite): ResPersonDiaryWrite = remoteDataSource.postPersonDiaryWrite(body)
+
+    override suspend fun getPersonDiaryRead(path: String): ResPersonDiaryRead = remoteDataSource.getPersonDiaryRead(path)
+
+    override suspend fun putPersonDiaryEdit(path: String, body: ReqPersonDiaryEdit): ResDiaryEdit = remoteDataSource.putPersonDiaryEdit(path, body)
+
+    override suspend fun deletePersonDiary(path: String, body: ReqDiaryDelete): ResDiaryDelete = remoteDataSource.deletePersonDiary(path, body)
+}
