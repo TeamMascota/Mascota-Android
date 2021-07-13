058a65ca6f1ef292bffc69723785137f9ccd87d9
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-07-13 21:20:56 +0900

    [ADD] #61 - Add Prfofile ViewModel Network

--- app/src/main/java/org/mascota/ui/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/org/mascota/ui/viewmodel/ProfileViewModel.kt
@@ -1,7 +1,21 @@
 package org.mascota.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import okhttp3.MultipartBody
 import org.mascota.data.repository.profile.ProfileRepository
+import org.mascota.util.Event
 
 class ProfileViewModel(private val profileRepository: ProfileRepository) : ViewModel() {
+    private val _nextBtnEnableEvent = MutableLiveData<Event<Boolean>>()
+    val nextBtnEnableEvent: LiveData<Event<Boolean>>
+        get() = _nextBtnEnableEvent
+
+    fun postBtnEnable(isEnable: Boolean) =
+        viewModelScope.launch { _nextBtnEnableEvent.postValue(Event(isEnable)) }
+
+    fun postRegisterPet(imageList: MutableList<MultipartBody.Part>) {}
 }
