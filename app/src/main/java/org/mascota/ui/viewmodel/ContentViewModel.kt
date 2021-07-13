11c4bcfa9da0ff75b61d277f60ba8ffdaa2c25e2
onpyeong <gsh447114@gmail.com>
2021-07-14 06:47:32 +0900

    [ADD] #75 - Add ContentViewModel Data Logic

--- app/src/main/java/org/mascota/ui/viewmodel/ContentViewModel.kt
+++ app/src/main/java/org/mascota/ui/viewmodel/ContentViewModel.kt
@@ -5,34 +5,23 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+import org.mascota.data.remote.model.response.content.ResContentDetail
 import org.mascota.data.repository.content.ContentRepository
-import org.mascota.ui.view.content.detail.data.datasource.ContentDetailDataSource
 import org.mascota.ui.view.content.detail.data.model.ContentDiaryInfoData
-import org.mascota.ui.view.content.detail.data.model.ContentMonthInfoData
 
-class ContentViewModel(private val contentRepository: ContentRepository, private val contentDetailDataSource: ContentDetailDataSource) : ViewModel() {
+class ContentViewModel(private val contentRepository: ContentRepository) : ViewModel() {
     private val _contentDetail = MutableLiveData<List<ContentDiaryInfoData>>()
     val contentDetail: LiveData<List<ContentDiaryInfoData>>
         get() = _contentDetail
 
-    private val _contentDetailMonth = MutableLiveData<List<ContentMonthInfoData>>()
-    val contentDetailMonth: LiveData<List<ContentMonthInfoData>>
-        get() = _contentDetailMonth
+    private val _resContentDetail = MutableLiveData<ResContentDetail>()
+    val resContentDetail : LiveData<ResContentDetail>
+        get() = _resContentDetail
 
-    fun getContentDetailInfo() = viewModelScope.launch {
-        runCatching { contentDetailDataSource.getContentDiaryInfoData() }
+    fun getContentDetail(chapterId: String) = viewModelScope.launch {
+        runCatching { contentRepository.getContentDetail(chapterId) }
             .onSuccess {
-                _contentDetail.postValue(it)
-            }
-            .onFailure {
-                it.printStackTrace()
-            }
-    }
-
-    fun getContentDetailMonthInfo() = viewModelScope.launch {
-        runCatching { contentDetailDataSource.getContentMonthInfoData()}
-            .onSuccess {
-                _contentDetailMonth.postValue(it)
+                _resContentDetail.postValue(it)
             }
             .onFailure {
                 it.printStackTrace()
