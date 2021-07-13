ea5e8d773d3adeb384fcbb50948be97b180c0dde
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-07-13 13:47:09 +0900

    [ADD] #51 - Add Remote BestMoment Model

---
+++ app/src/main/java/org/mascota/data/remote/model/response/rainbow/ResBestMoment.kt
@@ -0,0 +1,27 @@
+package org.mascota.data.remote.model.response.rainbow
+
+data class ResBestMoment(
+    //최고의 순간
+    val status: Int,
+    val success: Boolean,
+    val message: String,
+    val data: Data
+) {
+    data class Data(
+        val theBestMoments: List<TheBestMoment>
+    ) {
+        data class TheBestMoment(
+            val comment: String,
+            val feeling: Int,
+            val diaries: List<Diary>
+        ) {
+            data class Diary(
+                val chapter: Int,
+                val episode: Int,
+                val title: String,
+                val contents: String,
+                val date: String
+            )
+        }
+    }
+}
