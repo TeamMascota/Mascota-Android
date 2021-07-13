e0bf2858b2d43cd3c4f41f516626202d9bb0cbcf
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-07-13 13:37:16 +0900

    [ADD] #51 - Add Rainbow Service

---
+++ app/src/main/java/org/mascota/data/remote/api/RainbowService.kt
@@ -0,0 +1,65 @@
+package org.mascota.data.remote.api
+
+import org.mascota.data.remote.model.request.rainbow.ReqRainbowEpilogue
+import org.mascota.data.remote.model.response.rainbow.*
+import retrofit2.http.*
+
+interface RainbowService {
+    //GET Rainbow Home
+    @GET("rainbow/main/:{userId}/:{petId}")
+    suspend fun getRainbowHome(
+        @Path("userId") userId: String,
+        @Path("petId") petId: String
+    ): ResRainbowHome
+
+    //GET Rainbow Pet Select
+    @GET("rainbow/pet")
+    suspend fun getFarewellSelect(
+        @Path("id") id: String
+    ): ResFarewellSelect
+
+    //PUT Rainbow Content
+    @PUT("rainbow/pet/:{petId}")
+    suspend fun putRainbowContent(
+        @Path("petId") petId: String
+    ): ResRainbowContent
+
+    //DELETE Rainbow Quit
+    @DELETE("rainbow/pet/:{petId}")
+    suspend fun deleteFarewellQuit(
+        @Path("petId") petId: String
+    ): ResFarewellQuit
+
+    //GET Rainbow Book
+    @GET("rainbow/record/:{petId}")
+    suspend fun getRainbowBook(
+        @Path("userId") userId: String,
+        @Path("petId") petId: String
+    ): ResRainbowBook
+
+    //GET Rainbow Best Moment
+    @GET("rainbow/moment/:{userId}/:{petId}")
+    suspend fun getRainbowBestMoment(
+        @Path("userId") userId: String,
+        @Path("petId") petId: String
+    ): ResBestMoment
+
+    //GET Rainbow Pet Name
+    @GET("rainbow/parting/pet/:{petId}")
+    suspend fun getRainbowPetName(
+        @Path("petId") petId: String
+    ): ResPetName
+
+    //POST Rainbow Epilogue
+    @POST("rainbow/epilogue/:{userId}/:{petId}")
+    suspend fun postRainbowEpilogue(
+        @Body body: ReqRainbowEpilogue
+    ): ResRainbowEpilogue
+
+    //GET Sub Home
+    @GET("rainbow/moment/sub/:{petId}")
+    suspend fun getRainbowSubHome(
+        @Path("userId") userId: String,
+        @Path("petId") petId: String
+    ): ResRainbowSubHome
+}
