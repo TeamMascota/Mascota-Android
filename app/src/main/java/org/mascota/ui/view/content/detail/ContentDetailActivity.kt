a349c3ca924e05f13ce4fbe97bac8b9d5143a3df
onpyeong <gsh447114@gmail.com>
2021-07-13 19:14:50 +0900

    [FEAT] #59 - setClickListener and apply partialColor and Bold

--- app/src/main/java/org/mascota/ui/view/content/detail/ContentDetailActivity.kt
+++ app/src/main/java/org/mascota/ui/view/content/detail/ContentDetailActivity.kt
@@ -1,25 +1,24 @@
 package org.mascota.ui.view.content.detail
 
-import android.graphics.*
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.RecyclerView
+import android.content.Intent
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.mascota.R
 import org.mascota.databinding.ActivityContentDetailBinding
 import org.mascota.ui.base.BindingActivity
 import org.mascota.ui.view.content.detail.adapter.ContentDetailMonthAdapter
+import org.mascota.ui.view.diary.read.DiaryReadActivity
 import org.mascota.ui.viewmodel.ContentDetailViewModel
 
-class ContentDetailActivity : BindingActivity<ActivityContentDetailBinding>(R.layout.activity_content_detail) {
-    private val contentDetailViewModel : ContentDetailViewModel by viewModel()
+class ContentDetailActivity :
+    BindingActivity<ActivityContentDetailBinding>(R.layout.activity_content_detail) {
+    private val contentDetailViewModel: ContentDetailViewModel by viewModel()
     private lateinit var contentDetailMonthAdapter: ContentDetailMonthAdapter
 
     override fun initView() {
         getContentMonthInfo()
         initContentDetailMonthAdapter()
         observeContentDetailMonthInfo()
-
-        test()
+        setBackBtnClickListener()
     }
 
     private fun getContentMonthInfo() {
@@ -29,6 +28,9 @@ class ContentDetailActivity : BindingActivity<ActivityContentDetailBinding>(R.la
     private fun initContentDetailMonthAdapter() {
         contentDetailMonthAdapter = ContentDetailMonthAdapter()
         binding.rvContentMonth.adapter = contentDetailMonthAdapter
+        contentDetailMonthAdapter.setNavigateDiaryReadListener {
+            startActivity(Intent(this, DiaryReadActivity::class.java))
+        }
     }
 
     private fun observeContentDetailMonthInfo() {
@@ -37,7 +39,9 @@ class ContentDetailActivity : BindingActivity<ActivityContentDetailBinding>(R.la
         }
     }
 
-    private fun test() {
-
+    private fun setBackBtnClickListener() {
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
     }
 }
