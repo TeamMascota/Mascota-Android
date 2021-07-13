c71e62aa6b62cadce64dc3006c679e051d85d5ce
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-07-13 21:17:12 +0900

    [ADD] #61 - Add Profile DataSource

--- app/src/main/java/org/mascota/data/remote/datasource/profile/ProfileDataSource.kt
+++ app/src/main/java/org/mascota/data/remote/datasource/profile/ProfileDataSource.kt
@@ -1,5 +1,8 @@
 package org.mascota.data.remote.datasource.profile
 
+import okhttp3.MultipartBody
+import okhttp3.Request
+import okhttp3.RequestBody
 import org.mascota.data.remote.model.request.profile.ReqRegisterBook
 import org.mascota.data.remote.model.request.profile.ReqRegisterPet
 import org.mascota.data.remote.model.response.profile.ResRegisterBook
@@ -8,7 +11,7 @@ import retrofit2.http.Body
 
 interface ProfileDataSource {
     //POST 반려동물 등록
-    suspend fun postRegisterPet(body: ReqRegisterPet): ResRegisterPet
+    suspend fun postRegisterPet(body: ReqRegisterPet, images : List<MultipartBody.Part>): ResRegisterPet
 
     //POST 책 등록
     suspend fun postRegisterBook(body: ReqRegisterBook): ResRegisterBook
--- app/src/main/java/org/mascota/data/remote/datasource/profile/RemoteProfileDataSource.kt
+++ app/src/main/java/org/mascota/data/remote/datasource/profile/RemoteProfileDataSource.kt
@@ -1,5 +1,6 @@
 package org.mascota.data.remote.datasource.profile
 
+import okhttp3.MultipartBody
 import org.mascota.data.remote.api.ProfileService
 import org.mascota.data.remote.model.request.profile.ReqRegisterBook
 import org.mascota.data.remote.model.request.profile.ReqRegisterPet
@@ -7,8 +8,8 @@ import org.mascota.data.remote.model.response.profile.ResRegisterBook
 import org.mascota.data.remote.model.response.profile.ResRegisterPet
 
 class RemoteProfileDataSource(private val service: ProfileService) : ProfileDataSource {
-    override suspend fun postRegisterPet(body: ReqRegisterPet): ResRegisterPet =
-        service.postRegisterPet(body)
+    override suspend fun postRegisterPet(body: ReqRegisterPet, images: List<MultipartBody.Part>): ResRegisterPet =
+        service.postRegisterPet(body, images)
 
     override suspend fun postRegisterBook(body: ReqRegisterBook): ResRegisterBook =
         service.postRegisterBook(body)
