1875c70a7671141ee5dafabc92ce3d32a2cc7ebe
onpyeong <gsh447114@gmail.com>
2021-07-14 17:35:06 +0900

    [ADD] #78 - ContentViewModel Data Logic

--- app/src/main/java/org/mascota/ui/viewmodel/ContentViewModel.kt
+++ app/src/main/java/org/mascota/ui/viewmodel/ContentViewModel.kt
@@ -1,15 +1,19 @@
 package org.mascota.ui.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+import org.mascota.data.local.MascotaSharedPreference.getUserId
+import org.mascota.data.remote.model.request.content.ReqContent
 import org.mascota.data.remote.model.response.content.ResContentList
 import org.mascota.data.repository.content.ContentRepository
 import org.mascota.ui.view.content.detail.data.datasource.ContentDetailDataSource
 import org.mascota.ui.view.content.detail.data.model.ContentDiaryInfoData
 import org.mascota.ui.view.content.detail.data.model.ContentMonthInfoData
+import org.mascota.util.Event
 
 class ContentViewModel(private val contentRepository: ContentRepository, private val contentDetailDataSource: ContentDetailDataSource) : ViewModel() {
     private val _contentDetail = MutableLiveData<List<ContentDiaryInfoData>>()
@@ -24,8 +28,46 @@ class ContentViewModel(private val contentRepository: ContentRepository, private
     val resContentList: LiveData<ResContentList>
         get() = _resContentList
 
+
+    private val _chapterTitle = MutableLiveData<String>()
+
+    fun postChapterTitle(chapterTitle: String) {
+        _chapterTitle.postValue(chapterTitle)
+    }
+
+    private val _chapterId = MutableLiveData<String>()
+
+    fun postChapterId(chapterId: String) {
+        _chapterId.postValue(chapterId)
+    }
+
+    fun deleteContent() = viewModelScope.launch {
+        runCatching { contentRepository.deleteContent(requireNotNull(_chapterId.value)) }
+            .onSuccess { }
+            .onFailure {
+                it.printStackTrace()
+            }
+    }
+
+    fun putContentEdit() = viewModelScope.launch {
+        runCatching { contentRepository.putContentEdit(requireNotNull(_chapterId.value), ReqContent(requireNotNull(_chapterTitle.value)))}
+            .onSuccess {  }
+            .onFailure {
+                it.printStackTrace()
+            }
+    }
+
+    fun postContentAdd() = viewModelScope.launch {
+        runCatching { contentRepository.postContentAdd(getUserId(), ReqContent(requireNotNull(_chapterTitle.value)))}
+            .onSuccess {
+            }
+            .onFailure {
+                it.printStackTrace()
+            }
+    }
+
     fun getResContentList() = viewModelScope.launch {
-        runCatching { contentRepository.getContentList() }
+        runCatching { contentRepository.getContentList(getUserId()) }
             .onSuccess {
                 _resContentList.postValue(it)
             }
