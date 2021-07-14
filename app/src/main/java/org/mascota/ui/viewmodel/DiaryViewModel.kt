3313d1dead8f7253eae2a2c2cb4011103af8456c
onpyeong <gsh447114@gmail.com>
2021-07-15 06:54:36 +0900

    [ADD] #84 - Add DiaryViewModel Data Logic

--- app/src/main/java/org/mascota/ui/viewmodel/DiaryViewModel.kt
+++ app/src/main/java/org/mascota/ui/viewmodel/DiaryViewModel.kt
@@ -1,10 +1,13 @@
 package org.mascota.ui.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+import org.mascota.data.remote.model.response.diary.ResDiaryRead
+import org.mascota.data.remote.model.response.home.ResHomePart1
 import org.mascota.data.repository.diary.DiaryRepository
 import org.mascota.util.Event
 
@@ -16,6 +19,27 @@ class DiaryViewModel(private val diaryRepository: DiaryRepository) : ViewModel()
     private val _nextBtnEnableEvent = MutableLiveData<Event<Boolean>>()
     val nextBtnEnableEvent: LiveData<Event<Boolean>>
         get() = _nextBtnEnableEvent
+
+    private val _diaryReadPet = MutableLiveData<ResDiaryRead>()
+    val diaryReadPet: LiveData<ResDiaryRead>
+        get() = _diaryReadPet
+
+    private val _petDiaryId = MutableLiveData<String>()
+
+    fun postPetDiaryId(petDiaryId: String) {
+        _petDiaryId.postValue(petDiaryId)
+    }
+
+    fun getPetDiaryRead() = viewModelScope.launch {
+        runCatching { diaryRepository.getPetDiaryRead(requireNotNull("60edf95de5003a744892ce3d")) }
+            .onSuccess {
+                _diaryReadPet.postValue(it)
+                Log.d("server", it.toString())
+            }
+            .onFailure {
+                it.printStackTrace()
+            }
+    }
 
     fun postBtnEnable(isEnable: Boolean) =
         viewModelScope.launch { _nextBtnEnableEvent.postValue(Event(isEnable)) }
