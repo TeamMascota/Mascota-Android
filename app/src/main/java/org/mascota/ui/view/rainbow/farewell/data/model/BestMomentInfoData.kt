e967ce3539047d39578c8e603d057ad031de7f72
onpyeong <gsh447114@gmail.com>
2021-07-16 07:57:08 +0900

    [ADD] #88 - Add BestMomentAdapter Logic and ChangeType

---
+++ app/src/main/java/org/mascota/ui/view/rainbow/farewell/data/model/BestMomentInfoData.kt
@@ -0,0 +1,11 @@
+package org.mascota.ui.view.rainbow.farewell.data.model
+
+data class BestMomentInfoData(
+    val chapter: Int,
+    val episode: Int,
+    val title: String,
+    val contents: String,
+    val date: String,
+    val kind: Int,
+    val feeling: Int
+)
