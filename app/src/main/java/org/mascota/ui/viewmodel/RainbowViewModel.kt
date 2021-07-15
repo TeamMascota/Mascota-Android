bafd909b8c39bd9373666d02a70f68cd2537b42a
onpyeong <gsh447114@gmail.com>
2021-07-16 07:52:48 +0900

    [ADD] #88 - Add RainbowViewModel Data Logic for BestMoment

--- app/src/main/java/org/mascota/ui/viewmodel/RainbowViewModel.kt
+++ app/src/main/java/org/mascota/ui/viewmodel/RainbowViewModel.kt
@@ -1,5 +1,6 @@
 package org.mascota.ui.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,6 +8,7 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import org.mascota.data.local.MascotaSharedPreference.getPetId
 import org.mascota.data.local.MascotaSharedPreference.getUserId
+import org.mascota.data.remote.model.response.content.ResContentList
 import org.mascota.data.remote.model.response.rainbow.ResBestMoment
 import org.mascota.data.remote.model.response.rainbow.ResPetName
 import org.mascota.data.remote.model.response.rainbow.ResFarewellSelect
@@ -32,103 +34,15 @@ class RainbowViewModel(private val rainbowRepository: RainbowRepository, private
     val farewellAnimalList: LiveData<List<ResFarewellSelect.Data>>
         get() = _farewellAnimalList
 
-    /*private val _loveMoment =MutableLiveData<RainbowInfoData>()
-    val loveMoment : MutableLiveData<RainbowInfoData>
-            get() = _loveMoment
-    
-    private val _joyMoment = MutableLiveData<RainbowInfoData>()
-    val joyMoment : MutableLiveData<RainbowInfoData>
-    get() = _joyMoment
+    private val _resBestMoment = MutableLiveData<ResBestMoment>()
+    val resBestMoment: LiveData<ResBestMoment>
+        get() = _resBestMoment
 
-
-    private val _angryMoment = MutableLiveData<RainbowInfoData>()
-    val angryMoment : MutableLiveData<RainbowInfoData>
-    get() = _angryMoment
-
-
-    private val _usualMoment = MutableLiveData<RainbowInfoData>()
-    val usualMoment : MutableLiveData<RainbowInfoData>
-    get() = _usualMoment
-
-
-    private val _sadMoment = MutableLiveData<RainbowInfoData>()
-    val sadMoment : MutableLiveData<RainbowInfoData>
-    get() = _sadMoment
-
-    private val _boringMoment = MutableLiveData<RainbowInfoData>()
-    val boringMoment : MutableLiveData<RainbowInfoData>
-    get() = _boringMoment
-
-    fun getSadMoment() = viewModelScope.launch {
-        kotlin.runCatching { tempRainbowDataSource.getSadMomentData() }
+    fun getResRainBowBestMoment() = viewModelScope.launch {
+        runCatching { rainbowRepository.getRainbowBestMoment(getUserId(), getPetId()) }
             .onSuccess {
-                _sadMoment.postValue(it)
-            }
-            .onFailure {
-                it.printStackTrace()
-            }
-    }
-
-    fun getBoringMoment() = viewModelScope.launch {
-        kotlin.runCatching { tempRainbowDataSource.getBoringMomentData() }
-            .onSuccess {
-                _boringMoment.postValue(it)
-            }
-            .onFailure {
-                it.printStackTrace()
-            }
-    }
-
-    fun getUsualMoment() = viewModelScope.launch {
-        kotlin.runCatching { tempRainbowDataSource.getUsualMomentData() }
-            .onSuccess {
-                _usualMoment.postValue(it)
-            }
-            .onFailure {
-                it.printStackTrace()
-            }
-    }
-
-
-    fun getAngryMoment() = viewModelScope.launch {
-        kotlin.runCatching { tempRainbowDataSource.getAngryBestMomentData() }
-            .onSuccess {
-                _angryMoment.postValue(it)
-            }
-            .onFailure {
-                it.printStackTrace()
-            }
-
-
-    }
-    fun getJoyMent() = viewModelScope.launch {
-        kotlin.runCatching { tempRainbowDataSource.getJoyBestMomentData() }
-            .onSuccess {
-                _joyMoment.postValue(it)
-            }
-            .onFailure {
-                it.printStackTrace()
-            }
-    }
-
-
-    fun getloveoMent() = viewModelScope.launch {
-        kotlin.runCatching { tempRainbowDataSource.getLoveBestMomentData() }
-            .onSuccess {
-                _loveMoment.postValue(it)
-            }
-            .onFailure {
-                it.printStackTrace()
-            }
-    }*/
-
-
-    fun getBestMoment() = viewModelScope.launch {
-        //    //GET Rainbow Best Moment
-        //    suspend fun getRainbowBestMoment(userId: String, petId: String): ResBestMoment
-        kotlin.runCatching { rainbowRepository.getRainbowBestMoment(getUserId(), "60ed4359e5003a744892ce2b")}
-            .onSuccess {
-                _bestMoment.postValue(it)
+                _resBestMoment.postValue(it)
+                Log.d("server", it.toString())
             }
             .onFailure {
                 it.printStackTrace()
