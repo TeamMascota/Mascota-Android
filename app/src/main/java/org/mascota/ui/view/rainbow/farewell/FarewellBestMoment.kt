33222cf392c9f33b572c8be152505412fefa9eb1
onpyeong <gsh447114@gmail.com>
2021-07-16 07:54:56 +0900

    [FEAT] #88 - Add BestMoment Data Logic for subList

--- app/src/main/java/org/mascota/ui/view/rainbow/farewell/FarewellBestMoment.kt
+++ app/src/main/java/org/mascota/ui/view/rainbow/farewell/FarewellBestMoment.kt
@@ -1,9 +1,11 @@
 package org.mascota.ui.view.rainbow.farewell
 
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.mascota.R
+import org.mascota.data.remote.model.response.rainbow.ResBestMoment
 import org.mascota.databinding.FragmentFarewellBestMomentBinding
 import org.mascota.ui.base.BindingFragment
 import org.mascota.ui.view.rainbow.adapter.BestMomentAdapter
@@ -34,57 +36,67 @@ class FarewellBestMoment :
     val tvToday = currentDate.format(forMatter).toString()
 
     override fun initView() {
-        initData()
         initAdapter()
-        observeData()
         setToday()
+        getResBestMoment()
+        observeResRainBowBestMoment()
+    }
 
+    private fun observeResRainBowBestMoment() {
+        rainbowViewModel.resBestMoment.observe(viewLifecycleOwner){
+            it.data.apply {
+                pet.name.apply {
+                    binding.tvPetName.text = this
+                    binding.tvPetName2.text = this
+                    binding.tvPetName3.text = this
+                    binding.tvPetName4.text = this
+                    binding.tvPetName5.text = this
+                    binding.tvPetName6.text = this
+                }
+                loveAdapter.emo = Pair(pet.kind, 0)
+                joyAdapter.emo = Pair(pet.kind, 1)
+                usualAdapter.emo = Pair(pet.kind, 2)
+                angryAdapter.emo = Pair(pet.kind, 4)
+                sadAdapter.emo = Pair(pet.kind, 3)
+                boringAdapter.emo = Pair(pet.kind, 5)
+                loveAdapter.data = changePair(it.data.theBestMoments[0].diaries)
+                joyAdapter.data = changePair(it.data.theBestMoments[1].diaries)
+                usualAdapter.data = changePair(it.data.theBestMoments[2].diaries)
+                angryAdapter.data = changePair(it.data.theBestMoments[4].diaries)
+                sadAdapter.data = changePair(it.data.theBestMoments[3].diaries)
+                boringAdapter.data = changePair(it.data.theBestMoments[5].diaries)
+            }
+        }
+    }
+
+    private fun changePair(list: List<ResBestMoment.Data.TheBestMoment.Diary>)
+    : List<Pair<ResBestMoment.Data.TheBestMoment.Diary, ResBestMoment.Data.TheBestMoment.Diary>>{
+        val diaryPairList = arrayListOf<Pair<ResBestMoment.Data.TheBestMoment.Diary, ResBestMoment.Data.TheBestMoment.Diary>>()
+        var first : ResBestMoment.Data.TheBestMoment.Diary
+        var second :ResBestMoment.Data.TheBestMoment.Diary
+        for(i in list.indices step 2){
+            first = list[i]
+            second = list[i+1]
+            if(first == null){
+                first = ResBestMoment.Data.TheBestMoment.Diary(0,0,"","","")
+            }
+            if(second == null){
+                second = ResBestMoment.Data.TheBestMoment.Diary(0,0,"","","")
+            }
+            diaryPairList.add(Pair(first, second))
+        }
+        return diaryPairList
     }
 
     private fun setToday() {
         binding.tvToday.text = tvToday
     }
 
-
-    private fun initData() {
-        /*rainbowViewModel.getloveoMent()
-        rainbowViewModel.getJoyMent()
-        rainbowViewModel.getAngryMoment()
-        rainbowViewModel.getUsualMoment()
-        rainbowViewModel.getSadMoment()
-        rainbowViewModel.getBoringMoment()*/
-    }
-
-    private fun observeData() {
-        /*rainbowViewModel.loveMoment.observe(viewLifecycleOwner) {
-            loveAdapter.data = it.diaryList
-        }
-        rainbowViewModel.joyMoment.observe(viewLifecycleOwner) {
-            joyAdapter.data = it.diaryList
-        }
-        rainbowViewModel.angryMoment.observe(viewLifecycleOwner) {
-            angryAdapter.data = it.diaryList
-        }
-
-        rainbowViewModel.usualMoment.observe(viewLifecycleOwner) {
-            usualAdapter.data = it.diaryList
-        }
-        rainbowViewModel.sadMoment.observe(viewLifecycleOwner){
-            sadAdapter.data = it.diaryList
-        }
-        rainbowViewModel.boringMoment.observe(viewLifecycleOwner){
-            boringAdapter.data = it.diaryList
-        }*/
-
-        // 감정별로 분기 처리해주기 !!!!
-
-        //loveAdapter.data = it.data.theBestMoments
-        //joyAdapter.data = it.data.theBestMoments.
-
+    private fun getResBestMoment() {
+        rainbowViewModel.getResRainBowBestMoment()
     }
 
     private fun initAdapter() {
-
         loveAdapter = BestMomentAdapter()
         joyAdapter = BestMomentAdapter()
         angryAdapter = BestMomentAdapter()
@@ -99,11 +111,7 @@ class FarewellBestMoment :
             rcvUsual.adapter = usualAdapter
             rcvSad.adapter = sadAdapter
             rcvBoring.adapter = boringAdapter
-
         }
-
     }
-
-
 }
 
