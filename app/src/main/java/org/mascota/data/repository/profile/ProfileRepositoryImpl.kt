9a9930432d2e8ac7393eb953cf1ed78d010d3269
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-07-15 22:34:09 +0900

    [ADD] #61 - Add Repository

--- app/src/main/java/org/mascota/data/repository/profile/ProfileRepositoryImpl.kt
+++ app/src/main/java/org/mascota/data/repository/profile/ProfileRepositoryImpl.kt
@@ -4,12 +4,15 @@ import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import org.mascota.data.remote.datasource.profile.ProfileDataSource
 import org.mascota.data.remote.model.request.profile.ReqRegisterBook
-import org.mascota.data.remote.model.request.profile.ReqRegisterPet
 import org.mascota.data.remote.model.response.profile.ResRegisterBook
 import org.mascota.data.remote.model.response.profile.ResRegisterPet
+import org.mascota.ui.view.profile.data.model.Pets
 
 class ProfileRepositoryImpl(private val remoteDataSource: ProfileDataSource) : ProfileRepository {
-    override suspend fun postRegisterPet(body: ReqRegisterPet, images: RequestBody): ResRegisterPet =
+    override suspend fun postRegisterPet(
+        body: HashMap<String, RequestBody>,
+        images: List<MultipartBody.Part?>
+    ): ResRegisterPet =
         remoteDataSource.postRegisterPet(body, images)
 
     override suspend fun postRegisterBook(body: ReqRegisterBook): ResRegisterBook =
