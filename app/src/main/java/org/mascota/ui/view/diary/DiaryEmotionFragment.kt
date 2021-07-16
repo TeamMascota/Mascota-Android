0a0445acd3726ffd9fcf63b4db0418627a54f313
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-07-16 23:00:11 +0900

    [ADD] #94 - Add Diary Logic

--- app/src/main/java/org/mascota/ui/view/diary/DiaryEmotionFragment.kt
+++ app/src/main/java/org/mascota/ui/view/diary/DiaryEmotionFragment.kt
@@ -1,141 +1,145 @@
 package org.mascota.ui.view.diary
 
 
-
-import android.app.AlertDialog
+import android.app.Dialog
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import androidx.databinding.DataBindingUtil
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.mascota.R
-import org.mascota.databinding.DiaogDeleteBinding
+import org.mascota.data.remote.model.request.diary.ReqPetDiaryWrite
 import org.mascota.databinding.FragmentDiaryEmotionBinding
+import org.mascota.databinding.LayoutMascotaDialogBinding
 import org.mascota.ui.base.BindingFragment
 import org.mascota.ui.view.diary.adpter.SelectEmotionAdapter
 import org.mascota.ui.view.diary.adpter.SelectProfileAdapter
-import org.mascota.ui.view.diary.data.EmotionList
-import org.mascota.ui.view.diary.data.ProfileList
 import org.mascota.ui.viewmodel.DiaryViewModel
+import org.mascota.util.DialogUtil
+import org.mascota.util.StringUtil
+import org.mascota.util.extension.getColor
 
 
-class DiaryEmotionFragment : BindingFragment<FragmentDiaryEmotionBinding>(R.layout.fragment_diary_emotion){
-
-
-    private lateinit var diaogDeleteBinding : DiaogDeleteBinding
-    private val diaryViewModel : DiaryViewModel by sharedViewModel()
-
-
-
-    val selectEmotionaAdapter = SelectEmotionAdapter()
-    val selectProfileAdapter = SelectProfileAdapter()
-
-// 뷰모델 생성 -> 뷰모델 바이 어쩌구 써서,
-    //이닛뷰에서 뷰모델.postbtnenable
-
+class DiaryEmotionFragment :
+    BindingFragment<FragmentDiaryEmotionBinding>(R.layout.fragment_diary_emotion) {
+    private lateinit var deleteDialog: Dialog
+    private lateinit var deleteDialogBinding: LayoutMascotaDialogBinding
+    private val isCompleteEmotion = MutableList(4) { true }
+    private val diaryViewModel: DiaryViewModel by sharedViewModel()
+    private val selectEmotionAdapter = SelectEmotionAdapter()
+    private val selectProfileAdapter = SelectProfileAdapter()
+    private var selectedProfileNum = 0
 
     override fun initView() {
+        initDialogDataBinding()
+        initDialog()
+        initData()
         initAdapter()
-        initProfileData()
         clickProfile()
         showDeleteEmotion()
-        diaryViewModel.postBtnEnable(true)
+        observePetInfo()
     }
 
+    private fun initData() {
+        diaryViewModel.getPetInfo()
+    }
 
-    private fun showDeleteEmotion(){
-
-        selectEmotionaAdapter.setDeleteButtonClickListener {name, _ ->
-            // 다이얼로그 창 띄우기
-            showDeleteDialog(name)
+    private fun observePetInfo() {
+        diaryViewModel.petInfo.observe(viewLifecycleOwner) {
+            selectEmotionAdapter.data = it.data.pets
+            Log.d("", selectEmotionAdapter.itemCount.toString())
+            selectProfileAdapter.data = it.data.pets
         }
-
     }
 
-    private fun clickProfile(){
-        selectProfileAdapter.setItemClickListenr(object :
-            SelectProfileAdapter.OnItemClickListener{
-            override fun onClick(v: View, position: Int, item : String) {
+    private fun showDeleteEmotion() {
+        selectEmotionAdapter.setDeleteButtonClickListener { name, position ->
+            deleteDialogBinding.tvContent.text = StringUtil.setTextPartialBold(
+                0,
+                name.length,
+                "${name}(이)가 주인공에서 해제됩니다.\n기분을 삭제하시겠어요?"
+            )
+            showDeleteDialog(position)
+        }
+    }
 
-                Log.d("아이템명",item)
-
-                selectEmotionaAdapter.emotionList.add(EmotionList(pet_name = item,))
-                selectEmotionaAdapter.notifyDataSetChanged()
-                binding.rcvEmotion.visibility = View.VISIBLE
-
+    private fun clickProfile() {
+        binding.apply {
+            selectProfileAdapter.setItemClickListener { name, position ->
+                deleteDialogBinding.tvContent.text = StringUtil.setTextPartialBold(
+                    0,
+                    name.length,
+                    "${name}(이)가 주인공에서 해제됩니다.\n기분을 삭제하시겠어요?"
+                )
+                showDeleteDialog(position)
             }
         }
-        )
-
     }
 
-    private fun initAdapter(){
+    private fun initDialog() {
+        deleteDialog = DialogUtil.makeDialog(requireContext())
 
-        // 리싸이클러뷰에 어댑터 넣어주기
-        binding.rcvEmotion.adapter = selectEmotionaAdapter
+        DialogUtil.setDialog(deleteDialog, deleteDialogBinding.root)
+    }
+
+    private fun initDialogDataBinding() {
+        deleteDialogBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(requireContext()),
+            R.layout.layout_mascota_dialog,
+            null,
+            false
+        )
+
+        deleteDialogBinding.apply {
+            tvNext.text = getString(R.string.delete)
+            tvNext.setTextColor(getColor(R.color.maco_orange))
+            tvTitle.text = getString(R.string.emotion_delete)
+            tvQuit.setOnClickListener {
+                deleteDialog.dismiss()
+            }
+        }
+    }
+
+    private fun initAdapter() {
+        binding.rcvEmotion.adapter = selectEmotionAdapter
         binding.rcvProfile.adapter = selectProfileAdapter
 
-
-        binding.rcvProfile.setHasFixedSize(true)
-        binding.rcvEmotion.setHasFixedSize(true)
-
-    }
-
-    private fun initProfileData(){
-        selectProfileAdapter.profileList.clear()
-
-        //어댑터에 데이터 넣고 갱신하기
-        selectProfileAdapter.profileList.addAll(
-            listOf<ProfileList>(
-                ProfileList(
-                    tv_name = "고봉이",
-                    img_profile = R.drawable.cat
-                ),
-                ProfileList(
-                    tv_name = "치삼이",
-                    img_profile = R.drawable.cat2
-                ),
-
-                )
-        )
-
-
-        selectProfileAdapter.notifyDataSetChanged()
-        selectEmotionaAdapter.emotionList.clear()
-
-
-        binding.rcvEmotion.visibility = View.INVISIBLE
-
-
-    }
-
-
-    private fun showDeleteDialog(name : String) {
-        diaogDeleteBinding = DataBindingUtil.inflate(LayoutInflater.from(requireContext())
-            , R.layout.diaog_delete, null,false)
-
-
-        diaogDeleteBinding.tvPetName.text  = name
-
-        val diaolog_delete = AlertDialog.Builder(context,0).create()
-
-        diaolog_delete.apply {
-            setView(diaogDeleteBinding.root)
-            setCancelable(false)
-        }.show()
-
-
-
-        diaogDeleteBinding.btnCancel.setOnClickListener{ diaolog_delete.dismiss()}
-        diaogDeleteBinding.btnDelete.setOnClickListener{
-            binding.rcvEmotion.visibility = View.GONE
-            diaolog_delete.dismiss()
-
+        selectEmotionAdapter.setEmoButtonClickListener { id, position, kind ->
+            diaryViewModel.emotionList[position] = ReqPetDiaryWrite.Character(id, kind)
+            isCompleteEmotion[position] = true
+            diaryViewModel.postBtnEnable(isEnable())
         }
 
+        selectProfileAdapter.setItemShowListener {
+            selectEmotionAdapter.setSelectedList(it, true)
+            ++selectedProfileNum
+            isCompleteEmotion[it] = false
+            diaryViewModel.postBtnEnable(isEnable())
+            selectEmotionAdapter.notifyDataSetChanged()
+        }
     }
 
+    private fun isEnable(): Boolean {
+        return isCompleteEmotion[0] && isCompleteEmotion[1] && isCompleteEmotion[2] && isCompleteEmotion[3] && selectedProfileNum > 0
+    }
 
+    override fun onResume() {
+        super.onResume()
+        diaryViewModel.postBtnEnable(isEnable())
+    }
 
+    private fun showDeleteDialog(position: Int) {
+        deleteDialogBinding.tvNext.setOnClickListener {
+            deleteDialog.dismiss()
+            --selectedProfileNum
+            isCompleteEmotion[position] = true
+            diaryViewModel.postBtnEnable(isEnable())
+            selectEmotionAdapter.setSelectedList(position, false)
+            selectProfileAdapter.setSelectedList(position, false)
+            diaryViewModel.emotionList[position] = diaryViewModel.emptyEmotionData
+            selectEmotionAdapter.notifyDataSetChanged()
+            selectProfileAdapter.notifyDataSetChanged()
+        }
 
+        deleteDialog.show()
+    }
 }
